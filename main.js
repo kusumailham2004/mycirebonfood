3bca4eb9cf7089a88437493b7ee210da88b804b2
kusumailham2004 <kusumailham2004@gmail.com>
2022-06-18 22:21:15 +0700

    revisi180912

--- main.js
+++ main.js
@@ -53,3 +53,22 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " activeslide";
 }
+
+
+
+function dropFunction(){
+    document.getElementById("dropdownlokasi").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if(!event.target.matches('.dropbtn')){
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
